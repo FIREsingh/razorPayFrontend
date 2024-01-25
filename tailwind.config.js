d916d80ab98f920e3e029fd3b40f930e38739166
FIREsingh <manishaithani67@gmail.com>
2024-01-25 10:50:41 +0530

    added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        deepBlur : '#02042a'
+      }
+    },
+  },
+  plugins: [],
+}
+
